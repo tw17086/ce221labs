eb62ea745d273f69d8bd4fd9eda2c0c191788e91
tw17086 <44199484+tw17086@users.noreply.github.com>
2018-11-12 22:08:59 +0000

    Add files via upload

--- Time.cpp
+++ Time.cpp
@@ -52,16 +52,9 @@ Time Time::operator++(int n)    // postfix version
 
 bool Time::operator<(const Time& t) const
 {
-	if (hour < t.hour){ return true; }
-	else
-	{
-		if (min < t.min) { return true; }
-		else
-		{
-			if (sec < t.sec) { return true; }
-		}
-	}
-	return false;
+	if (hour != t.hour) return hour < t.hour;
+	else if (min != t.min) return min < t.min;
+	else return sec < t.sec;
 }
 
 bool Time::operator<=(const Time& t) const
@@ -87,24 +80,34 @@ Time Time::operator-(unsigned int i) const
 
 Time& Time::operator-=(unsigned int i)
 {	sec -= i;
-	min = (min + sec)/60;
-	cout << "min " << min << endl;
-	if (sec%=60 != 0) sec += 60;
-	hour = hour + min/60;
-	cout << "hour " << hour << endl;
-	if (min%=60 != 0) min += 60;
+	min -= abs(sec)/60;
+	hour -= abs(min)/60;
+
+	sec %= 60;
+	min %= 60;
 	hour %= 24;
+
+	if (sec < 0)
+	{	sec += 60;
+		min -= 1;
+	}
+	if (min < 0)
+	{	min += 60;
+		hour -= 1;
+	}
+	if (hour < 0) hour += 24;
 	return *this;
 }
 
 Time& Time::operator--()// prefix version
-{
-
+{	*this -= 1;
+  	return *this;
 }
 
 Time Time::operator--(int)// postfix version
-{
-
+{	Time tCopy(*this);
+	*this -= 1;
+  	return tCopy;
 }
 
 
@@ -122,23 +125,34 @@ bool operator>(const Time& t1, const Time& t2)
 
 bool operator>=(const Time& t1, const Time& t2)
 {
-	return (t1 > t2) || (t1 == t2);
+	return !(t1 < t2);
 }
-bool operator!=(const Time&, const Time&){
+bool operator!=(const Time& t1, const Time& t2){
+	return !(t1 == t2);
 }
-unsigned int operator%(const Time&, const Time&){
+unsigned int operator%(const Time& t1, const Time& t2){
   // t1%t2 should mean the elapsed time in sec from t1 to t2
   // e.g. if t1 is 10.45am and t2 is 11.55am t1%t2 would be 4200 (i.e. 70 mins)
   // if t1 is later in the day than t2 then you should assume t2 is on the next day
+
+  if (t2 >= t1){ return ((t2.hour-t1.hour)*3600 + (t2.min-t1.min)*60 + t2.sec-t1.sec);}
+  else { return (24*60*60 - ((t1.hour-t2.hour)*3600 + (t1.min-t2.min)*60 + t1.sec-t2.sec)); }
+
 
 }
 
 int main()
 {
-	Time t(22,0,0);
+	Time t(22,33,22);
 	cout << t << endl;
-	cout << (t - 3600) << endl;
-	cout << (t - 60) << endl;
-	cout << (t - 1) << endl;
-	cout << (t + 1) << endl;
+	cout << (t - 2430) << endl << (t--) << endl << (--t) << endl;
+	Time t2(12,34,56);
+	cout << (t != t2) << endl;
+	cout << (t < t2) << endl;
+	cout << (t2 >= t) << endl;
+	cout << (t >= t2) << endl;
+	cout << (t2%t) << "		" << (t%t2) << endl;
+	cout << (Time() + t2%t) << "	 " << (Time() + t%t2) << endl;
+	cout << (Time(10,45)%Time(11,55)) << endl;
+	cout << (Time() + Time(10,45)%Time(11,55)) << endl;
 }
